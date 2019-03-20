8afaca1f2f6e04c31161dc8ac97d931196f2108c
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-20 21:28:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["one", "two", "three"]
-  new_array = array.pop 
-  
+  new_array = array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
