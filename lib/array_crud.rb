bfd0bb67dfc6a9672ca7ae67059026163248c8bf
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-20 21:34:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,13 +26,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = []
   array.shift
 
 end
 
 def retrieve_element_from_index(array, index_number)
-array = ["one", "two", "three"]
 array.index[index_number]
 end
 
