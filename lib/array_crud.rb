561d36a5c9c4cc3796171c8fc03412622ab6fb27
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-20 21:29:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,13 +21,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["one", "two", "three"]
+  array = []
   array.pop
 
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["one", "two", "three"]
+  array = []
   array.shift
 
 end
